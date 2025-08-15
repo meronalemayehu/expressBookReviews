78d71a2d5775e5702d2081c48c494b16a5194702
Meron Alemayehu <mbalemayehu@gmail.com>
2025-08-15 16:12:01 -0400

    completed task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -59,6 +59,30 @@ public_users.get('/isbn/:isbn',function (req, res) {
   }
  });
   
+public_users.get('/async-isbn/:isbn', async function (req, res) {
+try {
+    const isbn = req.params.isbn;
+
+    let getBookByISBN = (isbn) => {
+    return new Promise((resolve, reject) => {
+        const book = books[isbn];
+        if (book) {
+        resolve(book);
+        } else {
+        reject("Book not found");
+        }
+    });
+    };
+
+    const bookData = await getBookByISBN(isbn);
+    return res.status(200).send(bookData);
+
+} catch (err) {
+    return res.status(404).json({ message: err });
+}
+});
+
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
