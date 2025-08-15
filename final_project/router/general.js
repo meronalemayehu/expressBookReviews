d10b4b7b5db1c4b4056830e615acfaf9e487e4a8
Meron Alemayehu <mbalemayehu@gmail.com>
2025-08-15 14:20:17 -0400

    completed task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -70,7 +70,14 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book && book.reviews) {
+    res.status(200).json(book.reviews);
+  } else {
+    res.status(404).json({ message: "Book not found or no reviews available." });
+  }
 });
 
 module.exports.general = public_users;
